6474c7134c6d1ff2dd34281ec9f5f60e315f4a12
Ryan Patrick <NinjaWithSpoons@gmail.com>
2020-07-23 00:56:48 -0600

    Data getter (#1)
    * function that gets historical data
    
    * add create a load data method for the yfinance data
    
    Co-authored-by: wolfejob <spayton.c@gmail.com>

--- untitled0.py
+++ untitled0.py
@@ -8,9 +8,10 @@ Created on Mon Jul 20 18:37:12 2020
 from keras.layers.core import Dense, Activation, Dropout
 from keras.layers.recurrent import LSTM
 from keras.models import Sequential
-import lstm, time
+import lstm, time, get_data
 
-X_train, y_train, X_test, y_test = lstm.load_data('TSLA.csv', 50, True)
+X_train, y_train, X_test, y_test = get_data.get_yahoo_finance_data('TSLA', 50, True)
+
 model = Sequential()
 model.add(LSTM(input_dim =1,
                output_dim=50,
@@ -28,7 +29,7 @@ start = time.time()
 model.compile(loss='mse', optimizer='rmsprop')
 print('compilation time:', time.time()-start)
 
-model.fit(X_train, y_train, batch_size=512, nb_epoch=1, validation_split=0.05)
+model.fit(X_train, y_train, batch_size=512, nb_epoch=5, validation_split=0.05)
 
 predictions = lstm.predict_sequences_multiple(model, X_test, 50, 50)
 lstm.plot_results_multiple(predictions, y_test, 50)
